1827a0467d53b5fdb3c4792cad9e6fa87c8c1f65
tejamanthena <manthena.tejavarma@gmail.com>
2021-08-09 12:36:01 +0200

    Modified msToHours Custom Pipe to display the time in correct format

--- src/app/ms-to-hours.pipe.ts
+++ src/app/ms-to-hours.pipe.ts
@@ -1,27 +1,21 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'msToHours'
+  name: 'msToHours',
 })
 
 export class MsToHoursPipe implements PipeTransform {
 
-  transform(milliseconds: number): string {
+  transform(time: number): string {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
 
-  let hours = milliseconds / (1000*60*60);
-  let absoluteHours = Math.floor(hours);
-  let hrs = absoluteHours > 9 ? absoluteHours : '0' + absoluteHours;
+    let hours = Math.floor((time / hour) % 24);
+    let minutes = Math.floor((time / minute) % 60);
+    let seconds = Math.floor((time / second) % 60);
 
-  let minutes = (hours - absoluteHours) * 60;
-  let absoluteMinutes = Math.floor(minutes);
-  let mins = absoluteMinutes > 9 ? absoluteMinutes : '0' + absoluteMinutes;
-
-  let seconds = (minutes - absoluteMinutes) * 60;
-  let absoluteSeconds = Math.floor(seconds);
-  let secs = absoluteSeconds > 9 ? absoluteSeconds : '0' + absoluteSeconds;
-
-
-  return `${hrs} H` + ' : ' + `${mins} M`;
-    
+    return `${hours} h` + ' : ' + `${minutes} m` + ' : ' + `${seconds} s`;
   }
+
 }
